3486c0942d71bf069b8b606d0b705efe04310833
encoderlee <encoderlee@gmail.com>
2026-03-31 02:39:45 +0800

    mailcow

--- email/mailcow.sh
+++ email/mailcow.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+set -euo pipefail
+if [ "$(id -u)" -ne 0 ]; then
+  echo "error: this script must be run as root."
+  exit 1
+fi
+apt update
+apt install -y git openssl curl gawk coreutils grep jq
+curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+systemctl enable --now docker
+apt update
+apt install docker-compose-plugin
+umask 0022
+cd /opt
+git clone https://github.com/mailcow/mailcow-dockerized
+cd mailcow-dockerized
+./generate_config.sh
+docker compose pull
+docker compose up -d
